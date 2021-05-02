38cf96ed40cdfe36904adf566be70d4e44475c93
xbersoy <burakersoy@msn.com>
2021-05-03 02:49:14 +0300

    validations moved into middleware functions

--- routes/records.js
+++ routes/records.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const recordController = require('../controllers/records');
 const responseManager = require('../middlewares/responseManager');
+const recordValidator = require('../validations/recordValidation');
+const validationFormatter = require('../middlewares/validationFormatter');
 
 router.post(
   '/',
+  recordValidator.recordParameterValidation,
+  validationFormatter,
   async (req, res, next) => {
     responseManager.async(req, res, next, recordController.getRecords(req));
   },
