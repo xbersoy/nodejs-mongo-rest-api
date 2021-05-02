1eee3b6a8487eb0da1c312872b77a990d0978706
xbersoy <burakersoy@msn.com>
2021-05-02 16:32:26 +0300

    generic response manager service created

---
+++ middlewares/responseManager.js
@@ -0,0 +1,48 @@
+const {GeneralError} = require('../util/errorTypes');
+
+module.exports.async = (req, res, next, promise) => {
+  promise
+    .then(responseData => {
+      res.status(200).json({
+        code: 0,
+        msg: "Success",
+        records: responseData
+      });
+    })
+    .catch(err => {
+      if (err instanceof GeneralError) {
+        return res.status(err.getStatusCode()).json({
+          code: 1,
+          msg: err.message
+        });
+      }
+
+      return res.status(500).json({
+        code: 1,
+        msg: err.message
+      });
+    });
+};
+
+module.exports.nonAsync = (req, res, next, fn) => {
+  try {
+    const data = fn(req);
+    res.status(200).json({
+      code: 0,
+      msg: "Success",
+      records: data
+    });
+  } catch (err) {
+    if (err instanceof GeneralError) {
+      return res.status(err.getStatusCode()).json({
+        code: 1,
+        msg: err.message
+      });
+    }
+
+    return res.status(500).json({
+      code: 1,
+      msg: err.message
+    });
+  }
+};
