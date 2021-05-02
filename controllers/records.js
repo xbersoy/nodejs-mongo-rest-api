e2def8dddf4f70f290666197188322f319e2922c
xbersoy <burakersoy@msn.com>
2021-05-03 02:41:39 +0300

    joi validation removed

--- controllers/records.js
+++ controllers/records.js
@@ -1,13 +1,6 @@
-const recordService = require('../services/records')
-const { registerValidation } = require('../validations/recordValidation');
+const recordService = require('../services/records');
 
-exports.getRecords = async (req) => {
-    const { validationError } = registerValidation(req.body);
-
-    if (validationError) {
-        return validationError.details[0].message;
-    }
-    
+exports.getRecords = async (req) => {    
     const { startDate, endDate, minCount, maxCount } = req.body;
     const records = await recordService.getRecords({startDate, endDate, minCount, maxCount});
     
