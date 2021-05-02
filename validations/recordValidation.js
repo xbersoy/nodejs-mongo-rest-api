90e1bf2f1308b0f1a161a22adc936aaaaa54311f
xbersoy <burakersoy@msn.com>
2021-05-03 02:51:21 +0300

    validations updated with express-validator

--- validations/recordValidation.js
+++ validations/recordValidation.js
@@ -1,16 +1,96 @@
-const Joi = require('joi')
-    .extend(require('@joi/date'));
+const {check} = require('express-validator');
+const formatter = require('../middlewares/validationFormatter');
 
-// validations 
-const registerValidation = (requestBody) => {
-    const recordsRequestSchema = Joi.object({
-        startDate: Joi.date().format('YYYY-MM-DD').required(),
-        endDate: Joi.date().format('YYYY-MM-DD').required(),
-        minCount: Joi.number().required(),
-        maxCount: Joi.number().required()
-    });
+exports.recordParameterValidation = [
+  checkStartDate(),
+  checkEndDate(),
+  checkMinCount(),
+  checkMaxCount(),
+  formatter,
+];
+
+function checkStartDate() {
+    return check('startDate')
+      .custom((value, {req}) => {
+        if (req.body.startDate === undefined) return true;
+        const valid = isDateValid(req.body.startDate);
+        return valid
+      })
+      .withMessage('startDate must be a valid date')
+      .bail()
+
+      .custom((value, {req}) => {
+        if (new Date(req.body.startDate) < new Date(req.body.endDate)) return true;
+      })
+      .withMessage('startDate should be less than endDate')
+      .bail();
+    }
     
-    return recordsRequestSchema.validate(requestBody);
-};
+    function checkEndDate() {
+      return check('endDate')
+      .custom((value, {req})=> {
+        if (req.body.endDate === undefined) return true;
+        const isValid = isDateValid(req.body.endDate);
+        return isValid
+      })
+      .withMessage('endDate must be a valid date')
+      .bail();
+  }
 
-module.exports.registerValidation = registerValidation;
+function checkMinCount() {
+  return check('minCount')
+    .exists()
+    .withMessage('minCount parameter should be present')
+    .bail()
+
+    .not()
+    .isEmpty()
+    .withMessage('minCount can not be empty!')
+    .bail()
+
+    .not()
+    .isString()
+    .withMessage('minCount should be a number, not a string')
+    .bail()
+
+    .custom((value, {req}) => {
+        if (req.body.minCount < req.body.maxCount) return true;
+      })
+    .withMessage('minCount should be greater than maxCount')
+    .bail()
+
+    .isNumeric({min: 0})
+    .withMessage('minCount should be a number')
+    .bail();
+}
+
+function checkMaxCount() {
+  return check('maxCount')
+    .exists()
+    .withMessage('maxCount parameter should be present')
+    .bail()
+
+    .not()
+    .isEmpty()
+    .withMessage('maxCount can not be empty!')
+    .bail()
+
+    .not()
+    .isString()
+    .withMessage('maxCount should be a number, not a string')
+    .bail()
+
+    .custom((value, {req}) => {
+        if (req.body.minCount < req.body.maxCount) return true;
+      })
+    .withMessage('maxCount should be greater than minCount')
+    .bail()
+
+    .isNumeric({min: 0})
+    .withMessage('maxCount should be a number')
+    .bail();
+}
+
+function isDateValid(...val) {
+  return !Number.isNaN(new Date(...val).valueOf());
+}
